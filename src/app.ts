989bf7903c4583334c472ca8e5d9c498a5f999e7
aperebel <alvaroperezbellon@gmail.com>
2025-02-15 11:43:23 +0100

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(helmet());
+
+export default app;
